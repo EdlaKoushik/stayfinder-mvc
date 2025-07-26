24448871387477f5419260cefcf47e705f82d2f3
EdlaKoushik <koushikedla01@gmail.com>
2025-07-26 09:46:58 +0530

    updated title

--- cloudConfig.js
+++ cloudConfig.js
@@ -11,7 +11,7 @@ cloudinary.config ({
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-      folder: 'wanderlust_DEV',
+      folder: 'stayfinder_DEV',
       allowedformats:["png","jpg","jpeg"],
       
     },
--- controllers/users.js
+++ controllers/users.js
@@ -15,7 +15,7 @@ module.exports.signup=async (req, res) => {
             if(err){
                 return next(err);
             }
-            req.flash("success", "Welcome to Wanderlust!");
+            req.flash("success", "Welcome to Stayfinder!");
             res.redirect("/listings");
         })
    
@@ -30,7 +30,7 @@ module.exports.renderLoginForm=(req,res)=>{
 }
 
 module.exports.login=async (req, res) => {
-    req.flash("success", "Welcome back to Wanderlust!");
+    req.flash("success", "Welcome back to Stayfinder!");
     let redirectUrl= res.locals.redirectUlr || "/listings";
     res.redirect(redirectUrl)
 }
